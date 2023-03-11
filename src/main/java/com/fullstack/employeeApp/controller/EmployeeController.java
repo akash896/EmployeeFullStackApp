bc19df0afd74ebc83583d5fe2aaeb3fd5ba32147
akash896 <akashmondal896@gmail.com>
2023-03-11 21:17:58 +0530

    base spring and react working

---
+++ src/main/java/com/fullstack/employeeApp/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.fullstack.employeeApp.controller;
+
+import com.fullstack.employeeApp.model.Employee;
+import com.fullstack.employeeApp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+}
---
+++ src/main/java/com/fullstack/employeeApp/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.fullstack.employeeApp.repository;
+
+import com.fullstack.employeeApp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
